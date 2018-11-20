571e130571f7913fbcd76c6589a041cf2aa71eb5
Marc-andre heroux <mah_937@hotmail.com>
2018-11-20 13:09:06 -0500

    Maps n shits

---
+++ maps/tileset.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="tileset" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="D:/Downloads/mininicular.png" width="128" height="128"/>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0" width="15.625" height="15.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.125" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="3" x="4.255" y="0.125" width="11.75" height="1.25"/>
+   <object id="4" x="0.125" y="1.125" width="15.875" height="14.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="0.125" width="14.875" height="15.75"/>
+  </objectgroup>
+ </tile>
+</tileset>
